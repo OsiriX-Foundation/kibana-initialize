41496819e452cbfaca7f6eceba8a73555d03678b
gerome12 <gerome.pasquier@gmail.com>
2021-03-01 15:58:22 +0100

    Update init.py

--- init.py
+++ init.py
@@ -8,9 +8,11 @@ def urlencode(data):
 headers = {"Content-Type": "application/json"}
 response = requests.post("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics222", headers=headers, data=open("rollup_job_kheops_metrics.json", "rb"))
 print(response.status_code)
+print(response.content)
 
 
 
 
 response = requests.post("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics/_start")
 print(response.status_code)
+print(response.content)
