ca20936db6efd1d68ec5e4aac89a1743db51f02f
gerome12 <gerome.pasquier@gmail.com>
2021-03-01 15:26:55 +0100

    Update init.py

--- init.py
+++ init.py
@@ -1,5 +1,4 @@
 import requests
 
-response = requests.get("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics/_start")
-
+response = requests.post("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics/_start")
 print(response.status_code)
