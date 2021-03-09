125472d17fa18c3e530d29bbc2ba6b45c3ef589f
gerome12 <gerome.pasquier@gmail.com>
2021-03-09 18:09:04 +0100

    Update init.py

--- init.py
+++ init.py
@@ -25,16 +25,21 @@ while True:
 print("Create and start rollup_job_kheops_metrics")
 response = requests.post("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics/_start")
 if response.status_code == 404:
-    headers = {"Content-Type": "application/json"}
-    response = requests.put("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics", headers=headers, data=open("rollup_job_kheops_metrics.json", "rb"))
-    print("Create")
-    print(response.status_code)
-    print(response.content)
-
-    response = requests.post("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics/_start")
-    print("Start")
-    print(response.status_code)
-    print(response.content) 
+    while True:
+        headers = {"Content-Type": "application/json"}
+        response = requests.put("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics", headers=headers, data=open("rollup_job_kheops_metrics.json", "rb"))
+        print(response.status_code)
+        print(response.content)
+        if response.status_code == 200:
+            print("Create")
+            break
+    while True:
+        response = requests.post("http://elasticsearch:9200/_rollup/job/rollup_job_kheops_metrics/_start")
+        print(response.status_code)
+        print(response.content)
+        if response.status_code == 200:
+            print("Start")
+            break
 else:
     print("rollup_job_kheops_metrics already exist and already started")
 
